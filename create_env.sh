f770e89924187a9fdc076f19ca785ee19a313bc4
Fabio <21972160+hipnologo@users.noreply.github.com>
2024-12-03 02:13:07 +0000

    Added create env

---
+++ create_env.sh
@@ -0,0 +1,10 @@
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate  # For Unix/macOS
+# venv\Scripts\activate    # For Windows Command Prompt
+# .\venv\Scripts\Activate.ps1  # For Windows PowerShell
+
+# Install the required packages
+pip install -r requirements.txt
